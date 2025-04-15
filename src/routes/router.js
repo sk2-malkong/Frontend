89d1eccf2a2a7a23d65a2cfd7225edb132ec3616
Park1234 <psj04100410262635@gmail.com>
2025-04-15 16:15:58 +0900

    라우터 수정

--- src/routes/router.js
+++ src/routes/router.js
@@ -8,8 +8,8 @@ import JoinContainer from '../pages/login/JoinContainer';
 import LoginContainer from '../pages/login/LoginContainer';
 import PostCreateContainer from '../pages/postcreate/PostCreateContainer';
 import PostDetailContainer from '../pages/postdetail/PostDetailContainer';
-import FindIdContainer from '../pages/login/findid/findIdContainer';
-import FindPasswordContainer from '../pages/login/findpassword/findPasswordContainer';
+import FindIdContainer from '../pages/login/findid/FindIdContainer';
+import FindPasswordContainer from '../pages/login/findpassword/FindPasswordContainer';
 
 const router = createBrowserRouter([
   {
