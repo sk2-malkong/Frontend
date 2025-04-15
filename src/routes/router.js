d0a736b9473652b361e830b4f0bde2308e0c7263
Park1234 <psj04100410262635@gmail.com>
2025-04-15 16:11:04 +0900

    아이디 찾기 비밀번호 재설정

--- src/routes/router.js
+++ src/routes/router.js
@@ -8,6 +8,8 @@ import JoinContainer from '../pages/login/JoinContainer';
 import LoginContainer from '../pages/login/LoginContainer';
 import PostCreateContainer from '../pages/postcreate/PostCreateContainer';
 import PostDetailContainer from '../pages/postdetail/PostDetailContainer';
+import FindIdContainer from '../pages/login/findid/findIdContainer';
+import FindPasswordContainer from '../pages/login/findpassword/findPasswordContainer';
 
 const router = createBrowserRouter([
   {
@@ -41,6 +43,14 @@ const router = createBrowserRouter([
     {
       path : "/login",
       element : <LoginContainer/>
+    },
+    {
+      path : "/findid",
+      element : <FindIdContainer/>
+    },
+    {
+      path : "/findpassword",
+      element : <FindPasswordContainer/>
     }
 ]);
 
