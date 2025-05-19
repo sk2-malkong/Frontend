908b0271eea521696166743ca1ba94ec434e9d8d
DBJ247 <gkalfkd@gmail.com>
2025-05-19 14:26:32 +0900

    api 소개 문서 페이지

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { createBrowserRouter, RouteObject } from 'react-router-dom'
+import { createBrowserRouter, RouteObject } from 'react-router-dom';
+
 import Layout from '../pages/layout/Layout';
 import MainContainer from '../pages/main/MainContainer';
 import IndexContainer from '../pages/layout/index/IndexContainer';
@@ -15,67 +16,80 @@ import LimitationLogContainer from '../pages/limitationlog/LimitationLogContaine
 import Search from '../pages/search/Search';
 import PostEditContainer from '../pages/postcreate/PostEditContainer';
 
+// ✅ 문서 레이아웃 및 문서 페이지 import
+import DocsLayout from '../pages/layout/index/docspages/DocsLayout';
+import StartPage from '../pages/layout/index/docspages/views/StartPage';
+import FAQPage from 'src/pages/layout/index/docspages/views/FAQPage';
+import AuthPage from 'src/pages/layout/index/docspages/views/AuthPage';
+import JwthashPage from 'src/pages/layout/index/docspages/views/Jwthash';
+import RequestPage from 'src/pages/layout/index/docspages/views/Request';
+import SecurityPage from 'src/pages/layout/index/docspages/views/Security';
+
 const routes: RouteObject[] = [
   {
     path: "/",
-    element: <Layout/>,
+    element: <Layout />,
     children: [
-      {
-        index: true,
-        element: <IndexContainer/>
-      },
-      {
-        path: '/main',
-        element: <MainContainer/>
-      },
-      {
-        path: '/mypage',
-        element: <MyPageContainer/>
-      },
-      {
-        path: "/profilefix",
-        element: <ProfileFixContainer/>
-      },
-      {
-        path: '/limitlog',
-        element: <LimitationLogContainer/>
-      },
-      {
-        path: '/postcreate',
-        element: <PostCreateContainer/>
-      },
-      {
-        path: '/post/:id',
-        element: <PostDetailContainer/>
-      },
-      {
-        path: '/edit/:id',
-        element: <PostEditContainer />
-      },
-      {
-        path: '/search',
-        element: <Search/>
-      }
+      { index: true, element: <IndexContainer /> },
+      { path: "/main", element: <MainContainer /> },
+      { path: "/mypage", element: <MyPageContainer /> },
+      { path: "/profilefix", element: <ProfileFixContainer /> },
+      { path: "/limitlog", element: <LimitationLogContainer /> },
+      { path: "/postcreate", element: <PostCreateContainer /> },
+      { path: "/post/:id", element: <PostDetailContainer /> },
+      { path: "/edit/:id", element: <PostEditContainer /> },
+      { path: "/search", element: <Search /> }
     ]
   },
   {
     path: "/join",
-    element: <JoinContainer/>
+    element: <JoinContainer />
   },
   {
     path: "/login",
-    element: <LoginContainer/>
+    element: <LoginContainer />
   },
   {
     path: "/findid",
-    element: <FindIdContainer/>
+    element: <FindIdContainer />
   },
   {
     path: "/findpassword",
-    element: <FindPasswordContainer/>
+    element: <FindPasswordContainer />
+  },
+  
+  {
+    path: "/docs",
+    element: <DocsLayout />,
+    children: [
+      {
+        path: "start",
+        element: <StartPage />
+      },
+      {
+        path: "faq",
+        element: <FAQPage />
+      },
+      {
+        path: "auth",
+        element: <AuthPage />
+      },
+      {
+        path: "jwthash",
+        element: <JwthashPage />
+      },
+      {
+        path: "request",
+        element: <RequestPage />
+      },
+      {
+        path: "security",
+        element: <SecurityPage />
+      },
+      
+    ]
   }
 ];
 
 const router = createBrowserRouter(routes);
-
 export default router;
